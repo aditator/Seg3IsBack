23869b20fb8bb2cb44f96910b06387c6cda7b29f
aditator <37847118+aditator@users.noreply.github.com>
2018-12-10 13:15:13 +0530

    Update Segmentor.R

--- Segmentor.R
+++ Segmentor.R
@@ -7,11 +7,12 @@ data(chr11ChIPseq,package="PeakSegDP")
 data(neuroblastoma,package="neuroblastoma")
 x=chr11ChIPseq$regions$chromStart
 y=neuroblastoma$profiles$logratio[1:20]
+z=c(1,2,2,1)
 
 #Performing Segmentation
 x_seg=Segmentor(x,model=1,Kmax=15)
 y_seg=Segmentor(y,model=2,Kmax=5)
-z_seg=Segmentor(c(1,2,2,1),model=1,Kmax=4)
+z_seg=Segmentor(z,model=1,Kmax=4)
 
 #Getting Change Points
 x_seg@breaks
@@ -26,3 +27,17 @@ abline(v=4,col="red")
 abline(v=7,col="red")
 abline(v=8,col="red")
 abline(v=16,col="red")
+
+#Plotting for case of 5 segments in Y
+plot(y)
+abline(v=1,col="blue")
+abline(v=7,col="blue")
+abline(v=8,col="blue")
+abline(v=11,col="blue")
+abline(v=20,col="blue")
+
+#Plotting for case of 3 segments in Z (c(1,2,2,1))
+plot(z)
+abline(v=1,col="green")
+abline(v=3,col="green")
+abline(v=4,col="green")
